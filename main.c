54470cf90f324741faced90510936c963343115b
trangng <trang.minhnguyen2610@gmail.com>
2025-05-22 00:30:28 -0400

    assignment 1

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!");
+int main(int args, char *argv[]) {
+    printf("Hello, World!\n");
+
+    printf("%d\n" , 27 + 42);
+
+    printf("%d\n" , args - 1);
 
 }
